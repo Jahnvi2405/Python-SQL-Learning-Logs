61ae4277a9ca34f94c815fef1528fc968c1185c1
Jahnvi2405 <63490528+Jahnvi2405@users.noreply.github.com>
2025-05-08 17:39:16 +0530

    Add files via upload

---
+++ Python_1.0.py
@@ -0,0 +1,43 @@
+# First python code
+print('Hello Python')
+
+# Check the Python Version
+import sys
+print(sys.version)
+
+# Note: You can press shift + enter to execute 1 line of code
+
+# Types of objects in python
+## integer = 11; float = 2.3; string = hello; you can use "type" function to see the type of thing
+
+type(2.2)
+type("hfurhf")
+
+# You can convert one type to another
+float(2) #this will give 2.0
+int(2.2) #this will give 2
+type(float(2)) #this will give float
+str("1.a")
+
+#Boolean data types are True and False
+True
+False
+type(True) #bool
+type("True") #string
+bool(1) #true
+
+type(4/2) #float
+type(4//2) #int
+
+#Expressions and variables: Expressions in Python can include operations among compatible types (e.g., integers and floats). For example, basic arithmetic operations like adding multiple numbers
+1+2
+23-4
+23*4-3/2
+23//4 #this gives integer
+
+# Variables
+x=23+34+56+2
+x
+y=2+3+5
+y
+x/(y^2)
